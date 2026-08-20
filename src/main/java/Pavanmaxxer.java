58f040717e8e6a1455a20a9c38ac277a7ebb0024
Pavan Madhu <pavanmadhup@gmail.com>
2026-08-20 16:53:41 +0800

    Track completed tasks with mark command

--- src/main/java/Pavanmaxxer.java
+++ src/main/java/Pavanmaxxer.java
@@ -4,6 +4,7 @@ public class Pavanmaxxer {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
         System.out.println("Hello! I'm Pavanmaxxer.");
@@ -18,8 +19,14 @@ public class Pavanmaxxer {
 
             if (input.equals("list")) {
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String statusIcon = isDone[i] ? "X" : " ";
+                    System.out.println((i + 1) + ".[" + statusIcon + "] " + tasks[i]);
                 }
+            } else if (input.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+                isDone[taskIndex] = true;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [X] " + tasks[taskIndex]);
             } else {
                 tasks[taskCount] = input;
                 taskCount++;
