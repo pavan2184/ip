08f3f88886f664a69a62150b8e2fa2c08d4d9af4
Pavan Madhu <pavanmadhup@gmail.com>
2026-08-20 16:36:35 +0800

    Add echo loop for Level 1

--- src/main/java/Pavanmaxxer.java
+++ src/main/java/Pavanmaxxer.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Pavanmaxxer {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Pavanmaxxer.");
         System.out.println("What can I do for you?");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(input);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
