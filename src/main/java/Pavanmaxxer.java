70c1997348b5c2849c367ea2cd3eb368f413e873
Pavan Madhu <pavanmadhup@gmail.com>
2026-08-20 17:03:04 +0800

    Support todo deadline and event commands

--- src/main/java/Pavanmaxxer.java
+++ src/main/java/Pavanmaxxer.java
@@ -33,6 +33,33 @@ public class Pavanmaxxer {
                 tasks[taskIndex].markAsNotDone();
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println("  " + tasks[taskIndex]);
+            } else if (input.startsWith("todo ")) {
+                String description = input.substring(5);
+                tasks[taskCount] = new Task(description, "T", "");
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount]);
+                taskCount++;
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+            } else if (input.startsWith("deadline ")) {
+                String[] deadlineParts = input.substring(9).split(" /by ", 2);
+                String description = deadlineParts[0];
+                String by = deadlineParts[1];
+                tasks[taskCount] = new Task(description, "D", " (by: " + by + ")");
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount]);
+                taskCount++;
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+            } else if (input.startsWith("event ")) {
+                String[] eventParts = input.substring(6).split(" /from ", 2);
+                String description = eventParts[0];
+                String[] timeParts = eventParts[1].split(" /to ", 2);
+                String from = timeParts[0];
+                String to = timeParts[1];
+                tasks[taskCount] = new Task(description, "E", " (from: " + from + " to: " + to + ")");
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount]);
+                taskCount++;
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
             } else {
                 tasks[taskCount] = new Task(input);
                 taskCount++;
