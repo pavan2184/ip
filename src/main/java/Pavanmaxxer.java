be38122d7de67eaf7aa26ea772bbb0cccf092722
Pavan Madhu <pavanmadhup@gmail.com>
2026-08-20 16:48:27 +0800

    Store and list tasks for Level 2

--- src/main/java/Pavanmaxxer.java
+++ src/main/java/Pavanmaxxer.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Pavanmaxxer {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("Hello! I'm Pavanmaxxer.");
         System.out.println("What can I do for you?");
@@ -14,7 +16,15 @@ public class Pavanmaxxer {
                 break;
             }
 
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
+            }
         }
 
         System.out.println("Bye. Hope to see you again soon!");
