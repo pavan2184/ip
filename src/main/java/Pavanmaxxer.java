54dc23969d2a70a49c8444bbcb18c8bf74151b0d
Pavan Madhu <pavanmadhup@gmail.com>
2026-08-20 16:55:12 +0800

    Represent task state in a Task class

--- src/main/java/Pavanmaxxer.java
+++ src/main/java/Pavanmaxxer.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
+/**
+ * Runs the Pavanmaxxer text-based task manager.
+ */
 public class Pavanmaxxer {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
-        boolean[] isDone = new boolean[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println("Hello! I'm Pavanmaxxer.");
@@ -19,21 +21,20 @@ public class Pavanmaxxer {
 
             if (input.equals("list")) {
                 for (int i = 0; i < taskCount; i++) {
-                    String statusIcon = isDone[i] ? "X" : " ";
-                    System.out.println((i + 1) + ".[" + statusIcon + "] " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
             } else if (input.startsWith("mark ")) {
                 int taskIndex = Integer.parseInt(input.substring(5)) - 1;
-                isDone[taskIndex] = true;
+                tasks[taskIndex].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [X] " + tasks[taskIndex]);
+                System.out.println("  " + tasks[taskIndex]);
             } else if (input.startsWith("unmark ")) {
                 int taskIndex = Integer.parseInt(input.substring(7)) - 1;
-                isDone[taskIndex] = false;
+                tasks[taskIndex].markAsNotDone();
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  [ ] " + tasks[taskIndex]);
+                System.out.println("  " + tasks[taskIndex]);
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,41 @@
+/**
+ * Represents a task and whether it has been completed.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Creates an incomplete task with the given description.
+     *
+     * @param description description of the task
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Returns the icon used to display the task's completion status.
+     *
+     * @return {@code X} when done, or a blank space otherwise
+     */
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    /** Marks this task as completed. */
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /** Marks this task as not completed. */
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
